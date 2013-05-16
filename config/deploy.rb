537d5639d7db87721d6a62adc873652fa1069dba
Jeremy Mortelette <mortelette.jeremy@gmail.com>
2013-05-17 00:55:23 +0200

    [error] re-update config for gaston symlink

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,8 +47,8 @@ namespace :deploy do
 
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/mongoid.yml #{release_path}/config/mongoid.yml"
-    sudo "ln -rnfs #{shared_path}/gaston #{current_path}/config/gaston"
-    sudo "ln -rnfs #{shared_path}/assets #{current_path}/public/assets"
+    sudo "ln -nfs #{shared_path}/gaston #{current_path}/config/gaston"
+    sudo "ln -nfs #{shared_path}/assets #{current_path}/public/assets"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
 
