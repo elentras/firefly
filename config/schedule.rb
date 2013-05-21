1e2d17bb9addb2cf6d572cb1cea5acc3abfaee2b
Jeremy Mortelette <mortelette.jeremy@gmail.com>
2013-05-21 21:41:14 +0200

    changes on whenever config

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,11 +19,6 @@
 
 # Learn more: http://github.com/javan/whenever
 set :output, "#{path}/log/cron.log"
-set :whenever_environment, defer { stage }
-set :whenever_identifier, defer { "#{application}_#{stage}" }
-set :whenever_command, "bundle exec whenever"
-set :whenever_variables, defer { "'environment=#{rails_env}&current_path=#{current_path}'" }
-require 'whenever/capistrano'
 
 every 1.minutes do
   rake "cron:update", environment: "production"
